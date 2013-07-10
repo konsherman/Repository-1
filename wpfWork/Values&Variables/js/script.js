8e5a4c29c69d50c67483ba0fd1bfd6aa81050664
Konner Sherman <konsherman@fullsail.edu>
2013-07-10 15:17:04 -0400

    values and variables

---
+++ wpfWork/Values&Variables/js/script.js
@@ -0,0 +1,17 @@
+var yearBorn=1955;
+/*
+console.log("Hello World");
+console.log(yearBorn);
+*/
+//alert("Hello World");
+var yearOfBirth = 1993;
+var age; //declaration
+age = 19;
+var isStudent = false;
+var firstName = "Konner";
+var phrase = 'I don\'t know \n You\'ll have to speak witht he professor'
+var dogNames = ["Konner", "James", "Deeb", "Xavier" ];  //indexes 0,1,2,3 starts at 0
+var num = 1;
+dogNames[4]="jdog"
+//console.log(dogNames[4])
+console.log(dogNames[num])
