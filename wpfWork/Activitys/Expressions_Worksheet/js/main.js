97c9dea338089df2e47327d68e1207058723ed8e
Konner Sherman <konsherman@fullsail.edu>
2013-07-15 14:59:52 -0400

    Expressions

---
+++ wpfWork/Activitys/Expressions_Worksheet/js/main.js
@@ -0,0 +1,32 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: KonnyK
+ * Date: 7/15/13
+ * Time: 2:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/*         <---!DOG AGE CALCULATOR!--->
+var age = prompt("Please enter the human years of your dog!"); // i did a promt so its like a dog calculator.
+console.log(age*7+" Dog years old");
+
+
+if (age<3){
+    console.log("You're young");
+}                                // i just added in a if/else statement..because that's what we learned today and i felt like practicing them.
+
+else{ console.log("You a old dog");
+}
+*/
+
+
+
+/*     <--!AVERAGE SHOPPING BILL!--->
+var week1 = 123; // all the vars are the givens like how much money was spent in week1,2 ect..
+var week2 = 145;
+var week3 = 120;
+var week4 = 75;
+var week5 = 254;
+
+console.log (Math.floor(week1+week2+week3+week4+week5/5));
+*/
