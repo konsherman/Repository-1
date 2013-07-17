55be3835a20082a8e7e8d0455c22f54749a73651
Konner Sherman <konsherman@fullsail.edu>
2013-07-17 14:16:57 -0400

    Expressions;

---
+++ wpfWork/Activitys/Expressions/js/main.js
@@ -0,0 +1,59 @@
+
+console.log("------ops------");
+var a = 2;
+a = a + 3;
+console.log(a);
+
+
+console.log("---------Age----------");
+var yearBorn = 1993;
+var age = 2013 - yearBorn;
+console.log(age);
+
+
+console.log("---------Area---------");
+//area of the triangle is half the width times the height.
+var width = 4;
+var height = 5;
+var area = width * height/2;
+console.log(area);
+console.log("-------%Modulo%-----");//remainder, whats left over. Can tell if a number is even or odd.
+var remainder = 32%10;
+console.log(remainder);
+
+console.log("-------Order of Opp-----");
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60
+var quiz4 = 80;
+//the average is the sum of the items divided by the number of items.
+var average = (quiz1+quiz2+quiz3+quiz4)/4;// the stuff in the () will be done first.
+console.log(average);
+
+
+console.log("------Arrays------");
+var orangeBins = [234,567,883];
+var total = orangeBins[0]+orangeBins[1] + orangeBins[2]
+console.log(total);
+
+console.log("------Concatenatin-----");
+
+//var firstName = "Konner";
+//var lastName = "Sherman";
+//var fullName = firstName+ " " + lastName;
+//console.log(fullName);
+var a = "6";
+var b = "7";
+var result = a+ " And " + b;
+console.log(result);
+
+console.log("------!Assignment Operators-------");
+
+var a = 3;
+a += 4; //same as writing a = a+4 (*,/,+,-)
+a --;//a=a-1//a -=1
+console.log(a);
+
+
+
+
